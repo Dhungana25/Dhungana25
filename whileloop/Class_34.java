8d2647728d1066937aefc163f80f5d55d5c811b7
Dhungana25 <sarmila.mehta73@gmail.com>
2024-04-17 21:42:50 -0500

    while loop assignments

---
+++ whileloop/Class_34.java
@@ -0,0 +1,16 @@
+package whileloop;
+
+public class Class_34{
+
+	public static void main(String[] args) 
+	{
+	int i=10;
+	while(i>4)
+	{
+		System.out.println("Apple");
+		i--;  //if u do i++ loop will never stop
+	}
+
+	}
+
+}
